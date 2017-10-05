5f5ddc99918db854e54432f83b6114c7972ccbc1
akanosenritu <akasen.org@gmail.com>
2017-10-05 18:33:47 +0900

    corrected some errors

--- main.py
+++ main.py
@@ -21,11 +21,14 @@ class UnblockRequest:
         self.user = None
         self.is_ipuser = False
         self.is_blocked = False
+        self.is_range_blocked = False
         self.sysop = None
         self.timestamp_blocked = None
         self.reason_blocked = None
         self.timestamp_submitted = None
         self.revid_submitted = None
+        self.iprange = None
+        self.expiry = None
 
         # identify the user who requested unblock
         self.identify_user()
@@ -56,11 +59,6 @@ class UnblockRequest:
                     break
             else:
                 break
-        '''
-        if submitter != self.user:
-            print(vars(self))
-            raise UnblockRequestError(f"{submitter} does not match {self.user}")
-        '''
         self.timestamp_submitted = timestamp_submitted
         self.revid_submitted = revid_submitted
 
@@ -71,22 +69,17 @@ class UnblockRequest:
                 self.reason_blocked = log["reason"]
                 self.timestamp_blocked = log["timestamp"]
                 self.is_blocked = True
+                self.iprange = log["user"]
+                self.expiry = log["expiry"]
                 break
         else:
             for log in self.site.blocks(users=self.user):
                 self.sysop = log["by"]
                 self.reason_blocked = log["reason"]
                 self.timestamp_blocked = log["timestamp"]
+                self.expiry = log["expiry"]
                 self.is_blocked = True
                 break
-        '''
-        for log in self.site.logevents(logtype="block", page="利用者:"+self.user):
-            self.sysop = log.user()
-            self.timestamp_blocked = log.timestamp()
-            self.reason_blocked = log.comment()
-            self.is_blocked = True
-            break
-        '''
 
 
 def gen_unblock_requests(site):
@@ -98,16 +91,16 @@ def gen_unblock_requests(site):
 
 
 class LogPage:
-    def __init__(self, page_name="User:Akasenbot/UnblockRequests", requests=list(), errors=list()):
+    def __init__(self, kwargs: dict, page_name="User:Akasenbot/UnblockRequests"):
         self.page_name = page_name
         self.rendered = None
-        self.requests = requests
-        self.errors = errors
+        self.kwargs = kwargs
 
     def render(self):
         env = jinja2.Environment(loader=jinja2.FileSystemLoader("./", encoding="utf-8"))
         template = env.get_template("logpage.tmp")
-        self.rendered = template.render(requests=requests, errors=errors, today=datetime.datetime.today().isoformat(), pagename=self.page_name)
+        self.kwargs.update({"today":datetime.datetime.today().isoformat(), "pagename":self.page_name})
+        self.rendered = template.render(self.kwargs)
 
     def save(self):
         with codecs.open("temp.txt", "w", "utf-8") as f:
@@ -117,15 +110,20 @@ class LogPage:
 if __name__ == "__main__":
     jawp = pywikibot.Site()
     requests = list()
+    range_blocks = list()
     errors = list()
     for request in gen_unblock_requests(jawp):
         req = UnblockRequest(jawp, request)
         if req.is_blocked:
-            requests.append(req)
+            if req.iprange:
+                range_blocks.append(req)
+            else:
+                requests.append(req)
         else:
             errors.append(req)
-
-    logpage = LogPage(requests=requests, errors=errors)
+    for request in requests:
+        print(vars(request))
+    logpage = LogPage({"requests":requests, "range_blocks":range_blocks, "errors":errors})
     logpage.render()
     logpage.save()
 
